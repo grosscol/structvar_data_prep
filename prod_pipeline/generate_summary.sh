065c02c641b993586ea2adf5755d0025c26aa6b1
Colin Gross <grosscol@gmail.com>
2025-12-02 10:04:21 -0500

    Dynamically subsample to reduce output size.   Use number of CRAMS to determine subsampling rate.

--- prod_pipeline/generate_summary.sh
+++ prod_pipeline/generate_summary.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Process structural variants list with crams annotated.
+#   Subsample alignments in variant region for each sample (hets & homs)
+#
+# Positional params:
+#  1. Region pssed to tabix
 
 # Expected use:
 #  generate_summary.sh chr1
@@ -8,12 +14,12 @@ REGION="${1:-chr1}"
 INPUT_FILE=sv_crams.tsv.gz
 CRAM_SUMMARIZER=/net/wonderland/home/grosscol/projects/het_hom_selector/build/cram_summarizer/bin/cram_summ
 REF_FILE=/net/topmed/working/atks/ref/hs38DH.fa
-OUTPUT_FILE="sv_summary_${REGION}.json"
+OUTPUT_FILE="results/sv_summary_${REGION}.json"
 
 # Results holding directory
 mkdir -p pipes
 mkdir -p results
-echo "[" > results/${OUTPUT_FILE}
+echo "[" > ${OUTPUT_FILE}
 
 # While reading input file, translate tabs to '@' to avoid consecutive whitespace delimiters.
 #  They would be collapsed to a single delimiter due to how Bash does word splitting.
@@ -22,16 +28,17 @@ IFS='@'
 ROWNUM=0
 while read -ra ROW; do
   if [[ ${ROWNUM} -gt 0 ]]; then
-    echo ',' >> results/${OUTPUT_FILE}
+    echo ',' >> ${OUTPUT_FILE}
   fi
 
-  echo "Processing ${ROWNUM}"
   IFS=';' read -ra CRAMS <<< "${ROW[9]}"
   CHR=${ROW[0]}
   START=${ROW[5]}
   END=${ROW[6]}
   SV_ID=${ROW[2]}
+  DOWNSAMPLE=$(( 5 * ${#CRAMS[@]} ))
 
+  echo "Processing ${ROWNUM} C:${#CRAMS[@]} S:${DOWNSAMPLE}"
 
   # Generate one pipe per cram and begin reading into it.
   PIPES=()
@@ -55,11 +62,12 @@ while read -ra ROW; do
   samtools merge -f -o "${PIPE_MERGE}" "${PIPES[@]}" &
 
   # Summarize merged bams, remove unusable reads, and annote.
+  # Reduce data size by reducing key name and using 1|0 instead of boolean
   # Append to output file
-  ${CRAM_SUMMARIZER} "${PIPE_MERGE}" |\
-   jq --compact-output --arg sv_id "${SV_ID}" --arg chr "${CHR}" \
-     --argjson pos ${START} --argjson stop ${END} -f trim_by_chrom.jq >> results/${OUTPUT_FILE}
-   #gzip > "results/trimmed_summ_${SV_ID}.json.gz"
+  ${CRAM_SUMMARIZER} -s "${DOWNSAMPLE}" "${PIPE_MERGE}" |\
+    jq --compact-output --arg sv_id "${SV_ID}" --arg chr "${CHR}" \
+      --argjson pos ${START} --argjson stop ${END} -f trim_by_chrom.jq |\
+    sed 's/is_reverse/rev/;s/true/1/;s/false/0/' >> ${OUTPUT_FILE}
 
   # Cleanup pipes
   for P in ${PIPES[@]}
@@ -71,7 +79,7 @@ while read -ra ROW; do
   let "ROWNUM = ROWNUM + 1"
 done < <(tabix "${INPUT_FILE}" "${REGION}" | tr $'\t' '@')
 
-echo -n "]" >> results/${OUTPUT_FILE}
+echo -n "]" >> ${OUTPUT_FILE}
 
-echo "gzipping results"
-gzip -f results/${OUTPUT_FILE}
+echo "gzipping results to ${OUTPUT_FILE}.gz"
+gzip -f ${OUTPUT_FILE}

84ebc54443412de376c7b47fb0ea617db5a286cf
Colin Gross <grosscol@gmail.com>
2026-04-21 11:35:58 -0400

    Use full dataset in initial processing

--- pipelines/initial/reshape_bcf_to_tsv.sh
+++ pipelines/initial/reshape_bcf_to_tsv.sh
@@ -9,28 +9,26 @@ mkdir -p ${RESULT_DIR}
 BCF_FULL=/net/topmed2/working/gt-release/exchange-area/structural.variant.merge/structural.variant.1.1.genotypes.bcf
 
 # Result output for mongo impoort
-TSV_20=${RESULT_DIR}/sv.1.1.info.chr20.tsv
+TSV_INFO=${RESULT_DIR}/sv.1.1.info.tsv
 
 # Intermediate retagged bcf output used by downstream pipelines
 BCF_TAG=${PROC_DATA_DIR}/sv.1.1.genotypes.retagged.bcf
-
 
 # Re-tag bcf to force AC and AF to match genotypes
 #   Remove variants where AC=0
 if [ ! -f ${BCF_TAG} ]; then
   bcftools plugin fill-tags ${BCF_FULL} -- -t AN,AC,AC_Het,AC_Hom,AF,F_MISSING,NS |\
-    bcftools view --min-ac 1 --output-type b > ${BCF_TAG} 
+    bcftools view --min-ac 1 --output-type b > ${BCF_TAG}
   bcftools index ${BCF_TAG}
 fi
-
 
 Q_FMT=\
 '%CHROM\t%POS\t%ID\t%REF\t%FILTER\t%INFO/AC\t%INFO/AC_Het\t%INFO/AC_Hom\t%INFO/AF\t%INFO/AN\t%INFO/CALLRATE\t'\
 '%INFO/DPCNT\t%INFO/DPCNToverlap\t%INFO/END\t%INFO/F_MISSING\t%INFO/GCPCT\t%INFO/GTCNT\t%INFO/NS\t'\
 '%INFO/POST\t%INFO/PRE\t%INFO/SVLEN\t%INFO/SVTYPE\n'
 
-if [ ! -f ${TSV_20} ] || [ ! -s ${TSV_20} ]; then
-  bcftools query -r chr20 -f "${Q_FMT}" "${BCF_TAG}" > "${TSV_20}"
+if [ ! -f ${TSV_INFO} ] || [ ! -s ${TSV_INFO} ]; then
+  bcftools query -f "${Q_FMT}" "${BCF_TAG}" > "${TSV_INFO}"
 fi
 
 echo "done"
